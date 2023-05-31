755156d7565f57b6d2c43dd6ba1f4b85c6380e71
andrey lazar <andreylazarmr@gmail.com>
2023-05-31 09:38:04 +0300

    task

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+
+string[] array = { "hello", "2", "world", ":-)" };
+//string[] array = {"1234", "1567", "-2", "computer science"};
+//string[] array = { "Russia", "Denmark", "Kazan"};
