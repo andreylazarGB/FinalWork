b720fbacbcc66fbdd742ea953808aa3d7db00c87
andrey lazar <andreylazarmr@gmail.com>
2023-05-31 15:18:58 +0300

    change array

--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@
 string[] array = { "hello", "2", "world", ":-)" };
 //string[] array = {"1234", "1567", "-2", "computer science"};
 //string[] array = { "Russia", "Denmark", "Kazan"};
+
+string[] GetArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        char[] arr = Array[i].ToCharArray();        
+        if (arr.Length > 3) Array[i] = "";
+    }
+    return Array;
+}
